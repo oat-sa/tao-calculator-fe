7e0e9642dfeb95dcd19828606a94377d1a80dab3
jsconan <jsconan@gmail.com>
2023-01-26 15:37:54 +0100

    chore: add index files

---
+++ src/core/index.js
@@ -0,0 +1,25 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2023 Open Assessment Technologies SA ;
+ */
+
+export { default as engineFactory } from './engine.js';
+export { default as mathsEvaluatorFactory } from './mathsEvaluator.js';
+export { default as calculatorTokenizerFactory } from './tokenizer.js';
+export { default as expressionHelper } from './expression.js';
+export { default as tokensHelper } from './tokens.js';
+export { default as terms } from './terms.js';
+export { default as types } from './types.js';
