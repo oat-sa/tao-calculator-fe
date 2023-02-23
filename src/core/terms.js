db061971a3491e73e9c7dfeacfef7705cbf9f24e
jsconan <jsconan@gmail.com>
2023-02-23 17:38:41 +0100

    feat: add a term for the percentage operator

--- src/core/terms.js
+++ src/core/terms.js
@@ -283,6 +283,24 @@ export const terms = {
         type: types.operator,
         exponent: false
     },
+    INC: {
+        label: symbols.plus,
+        value: 'inc',
+        type: types.operator,
+        exponent: false
+    },
+    DEC: {
+        label: symbols.minus,
+        value: 'dec',
+        type: types.operator,
+        exponent: false
+    },
+    PERCENT: {
+        label: '%',
+        value: '#',
+        type: types.operator,
+        exponent: false
+    },
 
     // Variables
     ANS: {
@@ -333,7 +351,7 @@ export const terms = {
     },
     ERROR: {
         label: 'Syntax error',
-        value: '#',
+        value: 'Syntax',
         type: types.error,
         exponent: false
     },
